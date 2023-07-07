fc5a970ed03474fd3430da9b171f8630c696e22e
Anatalimar Lemos <95979892+Anatalimar@users.noreply.github.com>
2023-07-07 13:41:01 -0400

    atualização - Mauricio
    Atualizado no dia 07072013 as 1340

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<style>
-		body {
-  background-image: url(imagens/backmauri2.png);
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-}
+	body {
+  		background-image: url(imagens/backmauri2.png);
+  		background-size: cover;
+  		background-position: center;
+  		background-repeat: no-repeat;
+  		background-attachment: fixed;
+	}
 
 	</style>
 
